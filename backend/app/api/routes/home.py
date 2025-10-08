822530565f754f945ee6ac19f2c2114e4e4b8ec3
jack <jacksaycus@gmail.com>
2025-10-08 12:12:38 +0900

    feat: add logging with loguru

--- backend/app/api/routes/home.py
+++ backend/app/api/routes/home.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
-
+from app.core.logging import get_logger
+logger = get_logger()
 router = APIRouter(prefix="/home")
 
 @router.get("/")
 def home():
+    logger.info("Home page accessed")
+    logger.debug("Home page accessed")
+    logger.error("Home page accessed")
+    logger.warning("Home page accessed")
+    logger.critical("Home page accessed")
     return {"message": "Welcome to the NextGen Bank API"}
---
+++ backend/app/core/logging.py
@@ -0,0 +1,28 @@
+import os
+from app.core.config import settings
+from loguru import logger
+logger.remove()
+
+LOG_DIR = os.path.join(os.path.dirname(__file__), "logs")
+
+LOG_FORMAT = (
+    "{time:YYYY-MM-DD HH:mm:ss.SSS} |"
+    "{level: <8} |"
+    "{name}: {function}:{line} - "
+    "{message}"
+)
+
+logger.add(
+    sink=os.path.join(LOG_DIR,"error.log"),
+    format=LOG_FORMAT,
+    level="DEBUG" if settings.ENVIRONMENT == "local" else "INFO",
+    filter=lambda record: record["level"].no <= logger.level("WARNING").no,
+    rotation="10MB",
+    retention="30 days",
+    compression="zip",
+    backtrace=True,
+    diagnose=True
+)
+
+def get_logger():
+    return logger
