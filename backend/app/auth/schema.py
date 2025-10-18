a855dbc48a271ed3b3b0ee41fa5b98aa849d2d4f
jack <jacksaycus@gmail.com>
2025-10-18 12:19:51 +0900

    feat: users can reset their passwords

--- backend/app/auth/schema.py
+++ backend/app/auth/schema.py
@@ -85,3 +85,32 @@ class OTPVerifyRequestSchema(SQLModel):
         min_length=6,
         max_length=6,
     )
+
+class PasswordResetRequestSchema(SQLModel):
+    email: EmailStr
+
+class PasswordResetConfirmSchema(SQLModel):
+    new_password: str = Field(
+        ...,
+        min_length=8,
+        max_length=40,
+    )
+    confirm_password: str = Field(
+        ...,
+        min_length=8,
+        max_length=40,
+    )
+
+    @field_validator("confirm_password")
+    def validator_password_match(cls, v, values):
+        if "new_password" in values.data and v != values.data["new_password"]:
+            raise HTTPException(
+                status_code = status.HTTP_400_BAD_REQUEST,
+                detail={
+                    "status": "error",
+                    "messsage": "Password do not match",
+                    "action": "Please ensure that the passwords you entered match",
+                },
+            )
+        return v
+    
