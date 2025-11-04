061252b1b92ccc7ca1e1a64cf0ed72e2dd94700e
jack <jacksaycus@gmail.com>
2025-11-04 21:24:30 +0900

    feat: add idempotency key model, transaction schema and utils

--- backend/app/transaction/schema.py
+++ backend/app/transaction/schema.py
@@ -57,3 +57,22 @@ class TransferRequestSchema(SQLModel):
     amount: Decimal = Field(ge=0, decimal_places=2)
     security_answer: str = Field(max_length=30)
     description: str = Field(max_length=250)
+
+class TransferOTPVerificationsSchema(SQLModel):
+    transfer_reference: str
+    otp: str = Field(min_length=6, max_length=6)
+
+class TransferResponseSchema(SQLModel):
+    status: str
+    message: str
+    data: dict | None = None
+
+class CurrencyConversionSchema(SQLModel):
+    amount: Decimal
+    from_currency: str
+    to_currency: str
+    exchange_rate: Decimal
+    original_amount: Decimal
+    converted_amount: Decimal
+    conversion_fee: Decimal = Field(default=Decimal("0.00"))
+    
---
+++ backend/app/transaction/utils.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timezone
+from typing import Optional
+
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from backend.app.core.logging import get_logger
+from backend.app.transaction.enums import (
+    TransactionFailureReason,
+    TransactionStatusEnum
+)
+from backend.app.transaction.models import Transaction
+
+logger = get_logger()
+
+async def mark_transaction_failed(
+        transaction: Transaction,
+        reason: TransactionFailureReason,
+        details: dict,
+        session: AsyncSession,
+        error_message: Optional[str] = None,
+) -> None:
+    try:
+        transaction.status = TransactionStatusEnum.Failed
+
+        transaction.failed_reason = reason.value
+
+        current_metadata = transaction.transaction_metadata or {}
+
+        failure_details ={
+            "reason": reason.value,
+            "timestamp": datetime.now(timezone.utc).isoformat(),
+            "error_message": error_message,
+            **details,
+        }
+
+        transaction.transaction_metadata = {
+            **current_metadata,
+            "failure_details" : failure_details,
+        }
+
+        session.add(transaction)
+
+        await session.commit()
+
+        await session.refresh(transaction)
+
+        logger.error(
+            f"Transaction {transaction.reference} failed",
+            extra={
+                "reference": transaction.reference,
+                "reason": reason.value,
+                "details": failure_details,
+            },
+        )
+    except Exception as e:
+        logger.error(f"Error marking transaction as failed: {e}")
+
+        raise
